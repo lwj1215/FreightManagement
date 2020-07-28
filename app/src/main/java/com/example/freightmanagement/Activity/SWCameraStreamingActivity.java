564d653de01b04767f6f3db6d2271cd1bbf6f5ca
lwj1215 <13121998393@163.com>
2020-07-28 14:21:50 +0800

    推流集成

--- app/src/main/java/com/example/freightmanagement/Activity/SWCameraStreamingActivity.java
+++ app/src/main/java/com/example/freightmanagement/Activity/SWCameraStreamingActivity.java
@@ -1,32 +1,55 @@
 package com.example.freightmanagement.Activity;
 
+import android.content.pm.ActivityInfo;
 import android.hardware.Camera;
+import android.media.AudioFormat;
+import android.os.Build;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.freightmanagement.Base.BaseActivity;
 import com.example.freightmanagement.R;
+import com.example.freightmanagement.Utils.CameraConfig;
 import com.example.freightmanagement.View.CameraPreviewFrameView;
 import com.qiniu.pili.droid.streaming.AVCodecType;
-import com.qiniu.pili.droid.streaming.AudioSourceCallback;
 import com.qiniu.pili.droid.streaming.CameraStreamingSetting;
 import com.qiniu.pili.droid.streaming.MediaStreamingManager;
-import com.qiniu.pili.droid.streaming.StreamStatusCallback;
-import com.qiniu.pili.droid.streaming.StreamingEnv;
+import com.qiniu.pili.droid.streaming.MicrophoneStreamingSetting;
+import com.qiniu.pili.droid.streaming.StreamingPreviewCallback;
 import com.qiniu.pili.droid.streaming.StreamingProfile;
-import com.qiniu.pili.droid.streaming.StreamingSessionListener;
 import com.qiniu.pili.droid.streaming.StreamingState;
-import com.qiniu.pili.droid.streaming.StreamingStateChangedListener;
-import java.net.URISyntaxException;
+import com.qiniu.pili.droid.streaming.SurfaceTextureCallback;
+import com.qiniu.pili.droid.streaming.WatermarkSetting;
+import com.qiniu.pili.droid.streaming.microphone.AudioMixer;
+
 import java.nio.ByteBuffer;
 import java.util.List;
 
-public class SWCameraStreamingActivity extends BaseActivity  implements StreamingStateChangedListener, StreamStatusCallback, AudioSourceCallback, StreamingSessionListener {
+public class SWCameraStreamingActivity extends StreamingBaseActivity implements StreamingPreviewCallback,
+        CameraPreviewFrameView.Listener,
+        SurfaceTextureCallback {
 
 
     CameraPreviewFrameView mCameraPreviewSurfaceView;
     private MediaStreamingManager mMediaStreamingManager;
     private StreamingProfile mProfile;
     private String TAG = "StreamingByCameraActivity";
+    private CameraConfig mCameraConfig;
+    private CameraStreamingSetting mCameraStreamingSetting;
+    private boolean mIsNeedFB;
+    private boolean mIsPreviewMirror;
+    private boolean mIsEncodingMirror;
+    private int mCurrentCamFacingIndex;
+    private TextView mCameraSwitchBtn;
+    private TextView mFaceBeautyBtn;
+    private boolean mIsPictureStreaming = false;
+    private Switcher mSwitcher = new Switcher();
+    private AudioMixer mAudioMixer;
+    private StreamingProfile mProfile1;
 
     @Override
     public int setLayoutResource() {
@@ -35,45 +58,126 @@ public class SWCameraStreamingActivity extends BaseActivity  implements Streamin
 
     @Override
     protected void onInitView() {
-        init();
+
     }
 
     @Override
     protected void onLoadData2Remote() {
+        mCameraStreamingSetting = buildCameraStreamingSetting();
+        mIsEncOrientationPort = true;
+        mIsNeedFB = mCameraConfig.mIsFaceBeautyEnabled;
+        mIsPreviewMirror = mCameraConfig.mPreviewMirror;
+        mIsEncodingMirror = mCameraConfig.mEncodingMirror;
+        mCurrentCamFacingIndex = mCameraConfig.mFrontFacing ? 1 : 0;
 
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setRequestedOrientation(mIsEncOrientationPort ? ActivityInfo.SCREEN_ORIENTATION_PORTRAIT : ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+
+        mCameraSwitchBtn = (TextView) bindView(R.id.camera_switch_btn);
+        mFaceBeautyBtn = (TextView) bindView(R.id.fb_btn);
+
+        mFaceBeautyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mIsNeedFB = !mIsNeedFB;
+                mMediaStreamingManager.setVideoFilterType(mIsNeedFB ?
+                        CameraStreamingSetting.VIDEO_FILTER_TYPE.VIDEO_FILTER_BEAUTY
+                        : CameraStreamingSetting.VIDEO_FILTER_TYPE.VIDEO_FILTER_NONE);
+                updateFBButtonText();
+            }
+        });
+
+
+        mCameraSwitchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isPictureStreaming()) {
+                    return;
+                }
+
+                mCameraSwitchBtn.removeCallbacks(mSwitcher);
+                mCameraSwitchBtn.postDelayed(mSwitcher, 100);
+            }
+        });
     }
 
-    private void init() {
-        //get form you server
-        String publishURLFromServer =  getIntent().getStringExtra("url");;
-
-        mCameraPreviewSurfaceView = findViewById(R.id.cameraPreview_surfaceView);
-        try {
-            //encoding setting
-            mProfile = new StreamingProfile();
-            mProfile.setVideoQuality(StreamingProfile.VIDEO_QUALITY_HIGH1)
-                    .setAudioQuality(StreamingProfile.AUDIO_QUALITY_MEDIUM2)
-                    .setEncodingSizeLevel(StreamingProfile.VIDEO_ENCODING_HEIGHT_480)
-                    .setEncoderRCMode(StreamingProfile.EncoderRCModes.QUALITY_PRIORITY)
-                    .setPublishUrl(publishURLFromServer);
-
-            //preview setting
-            CameraStreamingSetting camerasetting = new CameraStreamingSetting();
-            camerasetting.setCameraId(Camera.CameraInfo.CAMERA_FACING_BACK)
-                    .setContinuousFocusModeEnabled(true)
-                    .setCameraPrvSizeLevel(CameraStreamingSetting.PREVIEW_SIZE_LEVEL.MEDIUM)
-                    .setCameraPrvSizeRatio(CameraStreamingSetting.PREVIEW_SIZE_RATIO.RATIO_16_9);
-
-            //streaming engine init and setListener
-            mMediaStreamingManager = new MediaStreamingManager(this, mCameraPreviewSurfaceView, AVCodecType.SW_VIDEO_WITH_SW_AUDIO_CODEC);  // soft codec
-            mMediaStreamingManager.prepare(camerasetting, mProfile);
-            mMediaStreamingManager.setStreamingStateListener(this);
-            mMediaStreamingManager.setStreamingSessionListener(this);
-            mMediaStreamingManager.setStreamStatusCallback(this);
-            mMediaStreamingManager.setAudioSourceCallback(this);
-        } catch (Exception e) {
-            e.printStackTrace();
+    private void updateFBButtonText() {
+        if (mFaceBeautyBtn != null) {
+            mFaceBeautyBtn.setText(mIsNeedFB ? "开美颜" : "关美颜");
         }
+    }
+
+    private boolean isPictureStreaming() {
+        if (mIsPictureStreaming) {
+            Toast.makeText(SWCameraStreamingActivity.this, "is picture streaming, operation failed!", Toast.LENGTH_SHORT).show();
+        }
+        return mIsPictureStreaming;
+    }
+
+
+    private CameraStreamingSetting buildCameraStreamingSetting() {
+        mCameraConfig = (CameraConfig) getIntent().getSerializableExtra("CameraConfig");
+
+        CameraStreamingSetting cameraStreamingSetting = new CameraStreamingSetting();
+        cameraStreamingSetting.setCameraId(mCameraConfig.mFrontFacing ? Camera.CameraInfo.CAMERA_FACING_FRONT : Camera.CameraInfo.CAMERA_FACING_BACK)
+                .setCameraPrvSizeLevel(mCameraConfig.mSizeLevel)
+                .setCameraPrvSizeRatio(mCameraConfig.mSizeRatio)
+                .setFocusMode(mCameraConfig.mFocusMode)
+                .setContinuousFocusModeEnabled(mCameraConfig.mContinuousAutoFocus)
+                .setFrontCameraPreviewMirror(mCameraConfig.mPreviewMirror)
+                .setFrontCameraMirror(mCameraConfig.mEncodingMirror).setRecordingHint(false)
+                .setResetTouchFocusDelayInMs(3000)
+                .setBuiltInFaceBeautyEnabled(!mCameraConfig.mIsCustomFaceBeauty)
+                .setFaceBeautySetting(new CameraStreamingSetting.FaceBeautySetting(1.0f, 1.0f, 0.8f));
+
+        if (mCameraConfig.mIsFaceBeautyEnabled) {
+            cameraStreamingSetting.setVideoFilter(CameraStreamingSetting.VIDEO_FILTER_TYPE.VIDEO_FILTER_BEAUTY);
+        } else {
+            cameraStreamingSetting.setVideoFilter(CameraStreamingSetting.VIDEO_FILTER_TYPE.VIDEO_FILTER_NONE);
+        }
+
+        return cameraStreamingSetting;
+    }
+
+    @Override
+    protected void initStreamingManager() {
+        CameraPreviewFrameView cameraPreviewFrameView = (CameraPreviewFrameView) findViewById(R.id.cameraPreview_surfaceView);
+        mMediaStreamingManager = new MediaStreamingManager(this, cameraPreviewFrameView, AVCodecType.SW_VIDEO_WITH_SW_AUDIO_CODEC);
+        mProfile1 = new StreamingProfile();
+        mProfile1.setPictureStreamingResourceId(R.mipmap.pause_publish);
+        String url = "rtmp://pili-publish.qnsdk.com/sdk-live/2a9b9dae-c229-411c-bfc6-940bda942d47?e=1595833142&token=QxZugR8TAhI38AiJ_cptTl3RbzLyca3t-AAiH-Hh:ioKtdeyOnxdnEsheNQvhCbyvlBY=";
+
+        mProfile1.setPictureStreamingFilePath(url);
+
+        MicrophoneStreamingSetting microphoneStreamingSetting = null;
+        if (mAudioStereoEnable) {
+            /**
+             * Notice !!! {@link AudioFormat#CHANNEL_IN_STEREO} is NOT guaranteed to work on all devices.
+             */
+            microphoneStreamingSetting = new MicrophoneStreamingSetting();
+            microphoneStreamingSetting.setChannelConfig(AudioFormat.CHANNEL_IN_STEREO);
+        }
+        mMediaStreamingManager.prepare(mCameraStreamingSetting, microphoneStreamingSetting, buildWatermarkSetting(), mProfile1);
+        mMediaStreamingManager.setAutoRefreshOverlay(true);
+        if (mCameraConfig.mIsCustomFaceBeauty) {
+            mMediaStreamingManager.setSurfaceTextureCallback(this);
+        }
+        cameraPreviewFrameView.setListener(this);
+        mMediaStreamingManager.setStreamingSessionListener(this);
+        mMediaStreamingManager.setStreamStatusCallback(this);
+        mMediaStreamingManager.setAudioSourceCallback(this);
+        mMediaStreamingManager.setStreamingStateListener(this);
+    }
+
+    @Override
+    protected boolean startStreaming() {
+        return mMediaStreamingManager.startStreaming();
+    }
+
+    @Override
+    protected boolean stopStreaming() {
+        return mMediaStreamingManager.stopStreaming();
     }
 
     @Override
@@ -176,6 +280,77 @@ public class SWCameraStreamingActivity extends BaseActivity  implements Streamin
                 Log.e(TAG, "开启闪光灯");
                 break;
 
+        }
+    }
+
+    /**
+     * Accept only 32 bit png (ARGB)
+     *
+     * @return
+     */
+    private WatermarkSetting buildWatermarkSetting() {
+
+        WatermarkSetting watermarkSetting = new WatermarkSetting(this);
+        watermarkSetting.setResourceId(R.mipmap.ic_launcher);
+        watermarkSetting.setAlpha(100);
+        watermarkSetting.setSize(WatermarkSetting.WATERMARK_SIZE.MEDIUM);
+        watermarkSetting.setLocation(WatermarkSetting.WATERMARK_LOCATION.SOUTH_EAST);
+
+        return watermarkSetting;
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onZoomValueChanged(float factor) {
+        return false;
+    }
+
+    @Override
+    public boolean onPreviewFrame(byte[] bytes, int i, int i1, int i2, int i3, long l) {
+        return false;
+    }
+
+    @Override
+    public void onSurfaceCreated() {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(int i, int i1) {
+
+    }
+
+    @Override
+    public void onSurfaceDestroyed() {
+
+    }
+
+    @Override
+    public int onDrawFrame(int i, int i1, int i2, float[] floats) {
+        return 0;
+    }
+
+    private class Switcher implements Runnable {
+        @Override
+        public void run() {
+            mCurrentCamFacingIndex = (mCurrentCamFacingIndex + 1) % CameraStreamingSetting.getNumberOfCameras();
+            CameraStreamingSetting.CAMERA_FACING_ID facingId;
+            if (mCurrentCamFacingIndex == CameraStreamingSetting.CAMERA_FACING_ID.CAMERA_FACING_BACK.ordinal()) {
+                facingId = CameraStreamingSetting.CAMERA_FACING_ID.CAMERA_FACING_BACK;
+            } else if (mCurrentCamFacingIndex == CameraStreamingSetting.CAMERA_FACING_ID.CAMERA_FACING_FRONT.ordinal()) {
+                facingId = CameraStreamingSetting.CAMERA_FACING_ID.CAMERA_FACING_FRONT;
+            } else {
+                facingId = CameraStreamingSetting.CAMERA_FACING_ID.CAMERA_FACING_3RD;
+            }
+            Log.i(TAG, "switchCamera:" + facingId);
+            mMediaStreamingManager.switchCamera(facingId);
+
+            mIsEncodingMirror = mCameraConfig.mEncodingMirror;
+            mIsPreviewMirror = facingId == CameraStreamingSetting.CAMERA_FACING_ID.CAMERA_FACING_FRONT ? mCameraConfig.mPreviewMirror : false;
         }
     }
 }
--- app/src/main/java/com/example/freightmanagement/Fragment/MeetingFragment.java
+++ app/src/main/java/com/example/freightmanagement/Fragment/MeetingFragment.java
@@ -1,23 +1,30 @@
 package com.example.freightmanagement.Fragment;
 
 import android.content.Intent;
+import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 import com.example.freightmanagement.Activity.SWCameraStreamingActivity;
 import com.example.freightmanagement.Base.BaseFragment;
 import com.example.freightmanagement.R;
+import com.example.freightmanagement.Utils.CameraConfig;
 import com.example.freightmanagement.Utils.PermissionChecker;
 import com.example.freightmanagement.Utils.ToastUtils;
+import com.qiniu.pili.droid.streaming.CameraStreamingSetting;
 import com.qiniu.pili.droid.streaming.StreamingEnv;
 
 
 public class MeetingFragment extends BaseFragment {
 
     private PermissionChecker mPermissionChecker;
-   private String url="rtmp://pili-publish.qnsdk.com/sdk-live/2a9b9dae-c229-411c-bfc6-940bda942d47?e=1595833142&token=QxZugR8TAhI38AiJ_cptTl3RbzLyca3t-AAiH-Hh:ioKtdeyOnxdnEsheNQvhCbyvlBY=";
+    private String url = "rtmp://pili-publish.qnsdk.com/sdk-live/2a9b9dae-c229-411c-bfc6-940bda942d47?e=1595833142&token=QxZugR8TAhI38AiJ_cptTl3RbzLyca3t-AAiH-Hh:ioKtdeyOnxdnEsheNQvhCbyvlBY=";
+
     @Override
     protected int getLayoutResource() {
         return R.layout.fragment_meeting;
@@ -53,8 +60,26 @@ public class MeetingFragment extends BaseFragment {
                 intent.putExtra("INPUT_TEXT", "");
                 intent.putExtra("TRANSFER_MODE_QUIC", false);
                 intent.putExtra("url", url);
+                intent.putExtra("CameraConfig", buildCameraConfig());
                 startActivity(intent);
                 break;
         }
+    }
+
+    private CameraConfig buildCameraConfig() {
+        CameraConfig cameraConfig = new CameraConfig();
+        View root = getView();
+
+        cameraConfig.mFrontFacing = true;
+        cameraConfig.mSizeLevel = CameraStreamingSetting.PREVIEW_SIZE_LEVEL.MEDIUM;
+        cameraConfig.mSizeRatio = CameraStreamingSetting.PREVIEW_SIZE_RATIO.RATIO_16_9;
+        cameraConfig.mFocusMode = Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO;
+        cameraConfig.mIsFaceBeautyEnabled = true;
+        cameraConfig.mIsCustomFaceBeauty = false;
+        cameraConfig.mContinuousAutoFocus = true;
+        cameraConfig.mPreviewMirror = false;
+        cameraConfig.mEncodingMirror = false;
+
+        return cameraConfig;
     }
 }
