da497be6661128f0ef588e468736729ee3f0a290
lwj1215 <13121998393@163.com>
2020-08-06 14:45:30 +0800

    mvp

--- app/src/main/java/com/example/freightmanagement/Activity/SignUpActivity.java
+++ app/src/main/java/com/example/freightmanagement/Activity/SignUpActivity.java
@@ -8,8 +8,9 @@ import android.view.View;
 
 import com.example.freightmanagement.Base.BaseActivity;
 import com.example.freightmanagement.R;
+import com.example.freightmanagement.presenter.LogPresenter;
 
-public class SignUpActivity extends BaseActivity implements View.OnClickListener {
+public class SignUpActivity extends BaseActivity<LogPresenter> implements  LogPresenter.View,View.OnClickListener {
 
     @Override
     public int setLayoutResource() {
@@ -23,7 +24,7 @@ public class SignUpActivity extends BaseActivity implements View.OnClickListener
 
     @Override
     protected void onLoadData2Remote() {
-
+     mPresenter.getCode();//调用
     }
 
     @Override
@@ -34,5 +35,15 @@ public class SignUpActivity extends BaseActivity implements View.OnClickListener
                 finish();
                 break;
         }
+    }
+
+    @Override
+    public void getDataSuc() {//请求成功回调
+
+    }
+
+    @Override
+    protected LogPresenter onInitLogicImpl() {//实例化p层
+        return new LogPresenter();
     }
 }
---
+++ app/src/main/java/com/example/freightmanagement/Base/BaseApiConstants.java
@@ -0,0 +1,17 @@
+package com.example.freightmanagement.Base;
+
+
+/**
+ * @Deprecated 用来存放项目中所有的接口url
+ * @author: ztc
+ * 2019/8/12
+ * @describe:
+ */
+public class BaseApiConstants {
+
+    /**
+     * 账号登录
+     * 参数：username，userpass
+     */
+    public static final String API_LOG = "/login_password.php";
+}
